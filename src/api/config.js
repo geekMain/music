c1d6911f405b81a40b272ad4735ad2bbc2a7dd49
geekMain <45280785+geekMain@users.noreply.github.com>
2022-01-04 15:09:33 +0800

    Update config.js

--- src/api/config.js
+++ src/api/config.js
@@ -1,9 +1,9 @@
 let api_base_url = ''
 
 if (process.env.NODE_ENV === 'development') {
-  api_base_url = 'https://netease-cloud-music-api-hazel-tau.vercel.app/'
+  api_base_url = 'https://netease-cloud-music-api-hazel-tau.vercel.app'
 } else if (process.env.NODE_ENV === 'production') {
-  api_base_url = 'https://nicemusic-api.lxhcool.cn/'
+  api_base_url = 'https://netease-cloud-music-api-hazel-tau.vercel.app/'
 }
 
 export default { api_base_url }
